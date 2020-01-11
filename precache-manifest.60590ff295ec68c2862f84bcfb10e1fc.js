90e2bcddb41a4aaa161d5f2f7c91cea5dfcef066
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2020-01-11 16:33:07 -0300

    Updates

---
+++ precache-manifest.60590ff295ec68c2862f84bcfb10e1fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bbe9623cb18eec45d1744c1e3c1410",
+    "url": "/jonataspinto.github.io/index.html"
+  },
+  {
+    "revision": "28e1b0a3d65757fe3988",
+    "url": "/jonataspinto.github.io/static/css/main.72a4d9c1.chunk.css"
+  },
+  {
+    "revision": "a95fe5804a236407e340",
+    "url": "/jonataspinto.github.io/static/js/2.3006e0d6.chunk.js"
+  },
+  {
+    "revision": "e48b50fdce3fad5abda2e075623adca0",
+    "url": "/jonataspinto.github.io/static/js/2.3006e0d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28e1b0a3d65757fe3988",
+    "url": "/jonataspinto.github.io/static/js/main.720e3932.chunk.js"
+  },
+  {
+    "revision": "94061357605bfd462541",
+    "url": "/jonataspinto.github.io/static/js/runtime-main.3e2d026d.js"
+  }
+]);
