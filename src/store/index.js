1589f15104ec353b19a602e096beffc8b5097aaf
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2020-01-27 22:44:38 -0300

    chore(redux): configuracao de redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,18 @@
-import {createStore, combineReducers, applyMiddleware } from 'redux'
-import ReduxThunk from 'redux-thunk'
-import { userReducer } from './user/user.reducer'
-import { themeReducer } from './theme/theme.reducer'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import { userReducer } from "./user/user.reducer";
+import { themeReducer } from "./theme/theme.reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducers =  combineReducers({
-    data: userReducer,
-    theme: themeReducer
-})
+const rootReducers = combineReducers({
+  data: userReducer,
+  theme: themeReducer
+});
 
 const store = createStore(
-    rootReducers,
-    {},
-    applyMiddleware(ReduxThunk)
-)
+  rootReducers,
+  {},
+  composeWithDevTools(applyMiddleware(ReduxThunk))
+);
 
-export {store}
+export { store };
