af3475d4d5cd44619be7473039f07867b512daf9
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-03-30 16:39:01 -0300

    wip

---
+++ build/precache-manifest.ef2506771550c90868be600195d699f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f713ef79cbea55229d1d96c0f2600fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a322e02d6f8451e1cd87",
+    "url": "/static/css/main.72a4d9c1.chunk.css"
+  },
+  {
+    "revision": "d002c50b0e977ce8e0d6",
+    "url": "/static/js/2.3e1af4d5.chunk.js"
+  },
+  {
+    "revision": "f93f1024f4d8f2b55ef077cc16482eb1",
+    "url": "/static/js/2.3e1af4d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a322e02d6f8451e1cd87",
+    "url": "/static/js/main.aa1a0b1e.chunk.js"
+  },
+  {
+    "revision": "67f1193e49c3ddfcd99f",
+    "url": "/static/js/runtime-main.5b5ef6e3.js"
+  }
+]);
