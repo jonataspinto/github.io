409206ebfac7caf6aa3f7bec94a87b8723aa2c9a
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-03-30 16:40:59 -0300

    update master

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Header from './header'
-import {CardProjects} from './card'
-import { Button, ButtonIcon, SetThemeIcon} from './button'
-import Skills from './skills' 
-import About from './about'  
-
-
-export { Header, CardProjects, Button, ButtonIcon, SetThemeIcon, Skills, About}
--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-const api = axios.create({baseURL:'https://api.github.com/users/jonataspinto'})
-
-export const GetGithubProfile = async ()=>{
-   const res = await api.get()
-   return await res.data
-}
